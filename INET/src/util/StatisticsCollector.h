e948a2cbff92ae5e72264b01d5f8206e756ab80b
hoangnq <hoangnq@gmail.com>
2010-03-10 13:30:03 +0000

    important update: statistics object at global-level, drawable hopCountMeanVector, linkStressVector

---
+++ INET/src/util/StatisticsCollector.h
@@ -0,0 +1,61 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#ifndef __STATISTICSCOLLECTOR_H__
+#define __STATISTICSCOLLECTOR_H__
+
+#include <omnetpp.h>
+
+/**
+ * TODO - Generated class
+ */
+class StatisticsCollector : public cSimpleModule
+{
+  protected:
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+    virtual void finish();
+
+  private:
+    int stressSum;
+    int numPhysicalLink;
+    cLongHistogram hopCountStats;
+    cOutVector hopCountVector;
+    cOutVector linkStressVector;
+
+    cMessage *timerMsg;
+    simtime_t statisticsPeriod;
+
+  public:
+
+	  void addToStressSum(int num);
+
+      void incStressSum(){stressSum++;};
+
+      double getLinkStress();
+
+      void setNumPhysicalLink(int num);
+
+      int getNumPhysicalLink(){return numPhysicalLink;};
+
+      void calculateNumPhysicalLink();
+
+      void collectHopCount(int hopCount);
+
+      ~StatisticsCollector();
+
+};
+
+#endif
