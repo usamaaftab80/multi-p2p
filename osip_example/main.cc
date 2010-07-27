5292f0b5622761374cc12dcd98428405a2ca6c82
tuantq84 <tuantq84@67113970-6624-11de-9f54-6d4fae1110e3>
2010-07-27 14:37:41 +0000

    EXOSIP

--- osip_example/main.cc
+++ osip_example/main.cc
@@ -4,10 +4,23 @@
  *  Created on: Jul 20, 2010
  *      Author: admin
  */
-#include "HOSIP.h"
+#include "EXOSIP.h"
+#include "pthread.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-	HOSIP * o = new HOSIP();
-	o->principe();
+	//HOSIP * o = new HOSIP();
+	//o->principe();
+	EXOSIP *o = new EXOSIP();
+	int i = 0;
+	i = o->initsip();
+	//int i = o->initsip();
+	//int i = o->initsip();
+	//o->initsip();
+	//pthread_t thread_id;
+	//thread_id = (pthread_t)malloc(sizeof(pthread_t));
+	//pthread_create(&thread_id, NULL, &(o->listen), NULL);
+	//pthread_create(&thread_id,NULL, &o->listen(),NULL);
 	return 0;
 }
